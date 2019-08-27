fb0253e655e5f86ad3df60fea5f9e58d94c4a377
amnisia <philip.pavlov@hotmail.com>
2019-08-27 11:56:13 -0500

    Added all functionality requested by Moe, updated front and back end, TODO: fix math

--- src/App.js
+++ src/App.js
@@ -13,17 +13,17 @@ class App extends Component {
     state = {
         fields: {},
 
-        cpu: "",
-        ram: "",
-        disk: "",
+        cpu: 0,
+        ram: 0,
+        disk: 0,
 
-        BMHostsOverall: "",
-        costPerMonthBM: "",
-        costPerMonthSW_Windows: "",
-        costPerMonthSW_SGD: "",
-        costPerMonthManagementVM: "",
-        totalPerMonth: "",
-        totalPerUserPerMonth: ""
+        BMHostsOverall: 0,
+        costPerMonthBM: 0,
+        costPerMonthSW_Windows: 0,
+        costPerMonthSW_SGD: 0,
+        costPerMonthManagementVM: 0,
+        totalPerMonth: 0,
+        totalPerUserPerMonth: 0
     };
 
 
@@ -43,18 +43,34 @@ class App extends Component {
 
         this.setState({fields});
 
-        cpu = (fields.numPowerUsers * data.PowerCPUs) + (fields.numKnowledgeUsers * data.KnowledgeCPUs) + (fields.numTaskUsers * data.TaskCPUs) +
-            (fields.numCustomUsers * fields.customCPUs);
+        cpu = ((fields.numPowerUsers * data.PowerCPUs) + (fields.numKnowledgeUsers * data.KnowledgeCPUs) +
+            (fields.numTaskUsers * data.TaskCPUs) +
+            (fields.numCustomUsers * fields.customCPUs) +
+            (fields.numCustomUsers2 * fields.customCPUs2) +
+            (fields.numCustomUsers3 * fields.customCPUs3) +
+            (fields.numCustomUsers4 * fields.customCPUs4));
 
-        ram = (fields.numPowerUsers * data.PowerRAM) + (fields.numKnowledgeUsers * data.KnowledgeRAM) + (fields.numTaskUsers * data.TaskRAM) +
-            (fields.numCustomUsers * fields.customRAM);
+        ram = ((fields.numPowerUsers * data.PowerRAM) + (fields.numKnowledgeUsers * data.KnowledgeRAM) +
+            (fields.numTaskUsers * data.TaskRAM) +
+            (fields.numCustomUsers * fields.customRAM) +
+            (fields.numCustomUsers2 * fields.customRAM2) +
+            (fields.numCustomUsers3 * fields.customRAM3) +
+            (fields.numCustomUsers4 * fields.customRAM4));
 
-        disk = (fields.numPowerUsers * data.PowerDisk) + (fields.numKnowledgeUsers * data.KnowledgeDisk) + (fields.numTaskUsers * data.TaskDisk) +
-            (fields.numCustomUsers * fields.customDisk);
+        disk = ((fields.numPowerUsers * data.PowerDisk) + (fields.numKnowledgeUsers * data.KnowledgeDisk) +
+            (fields.numTaskUsers * data.TaskDisk) +
+            (fields.numCustomUsers * fields.customDisk) +
+            (fields.numCustomUsers2 * fields.customDisk2) +
+            (fields.numCustomUsers3 * fields.customDisk3) +
+            (fields.numCustomUsers4 * fields.customDisk4));
 
         var bmHostsByCPU = (cpu / BMHostoCPUVMsavailable);
         var bmHostsbyRAM = (ram / BMHostRAMavailable);
-        var totalUsers = (Number(fields.numPowerUsers) + Number(fields.numKnowledgeUsers) + Number(fields.numTaskUsers) + Number(fields.numCustomUsers));
+        var totalUsers = (Number(fields.numPowerUsers) + Number(fields.numKnowledgeUsers) + Number(fields.numTaskUsers)
+            + Number(fields.numCustomUsers)
+            + Number(fields.numCustomUsers2)
+            + Number(fields.numCustomUsers3)
+            + Number(fields.numCustomUsers4));
 
         BMHostsOverall = Math.max(bmHostsByCPU, bmHostsbyRAM);
         costPerMonthBM = (BMHostsOverall * BMHostCostHrDenseIO * OCIHoursinamonth * BMHostCPUtotal);
@@ -137,19 +153,19 @@ class App extends Component {
                     <tr>
                         <th>Bare Metal Hosts</th>
                         <th>Bare Metal $/month</th>
-                        <th>SW Windows $/month</th>
-                        <th>SW SGD $/month</th>
+                        <th>Windows License $/month</th>
+                        <th>SGD License $/month</th>
                         <th>Management VM $/month</th>
                         <th>Total $/month</th>
-                        <th>Total Total $/month/user</th>
+                        <th>Total $/month/user</th>
                     </tr>
                     </thead>
                     <tbody>
                     <tr>
                         <td data-label="Bare Metal Hosts">{this.state.BMHostsOverall}</td>
                         <td data-label="Bare Metal $/month">{this.state.costPerMonthBM}</td>
-                        <td data-label="SW Windows $/month">{this.state.costPerMonthSW_Windows}</td>
-                        <td data-label="SW SGD $/month">{this.state.costPerMonthSW_SGD}</td>
+                        <td data-label="Windows License $/month">{this.state.costPerMonthSW_Windows}</td>
+                        <td data-label="SGD License $/month">{this.state.costPerMonthSW_SGD}</td>
                         <td data-label="Management VM $/month">{this.state.costPerMonthManagementVM}</td>
                         <td data-label="Total $/month">{this.state.totalPerMonth}</td>
                         <td data-label="Total $/month/user">{this.state.totalPerUserPerMonth}</td>
--- src/Form.js
+++ src/Form.js
@@ -12,17 +12,45 @@ export default class FormExampleFieldControl extends Component  {
         email: "",
         password: "",
 
-        numPowerUsers: "",
-        numKnowledgeUsers: "",
-        numTaskUsers: "",
-        numCustomUsers: "",
+        numPowerUsers: 0,
+        numKnowledgeUsers: 0,
+        numTaskUsers: 0,
 
-        customCPUs: "",
-        customRAM: "",
-        customDisk: "",
+        numCustomUsers: 0,
+        customCPUs: 0,
+        customRAM: 0,
+        customDisk: 0,
 
-        customAdditional: []
+        numCustomUsers2: 0,
+        customCPUs2: 0,
+        customRAM2: 0,
+        customDisk2: 0,
+
+        numCustomUsers3: 0,
+        customCPUs3: 0,
+        customRAM3: 0,
+        customDisk3: 0,
+
+        numCustomUsers4: 0,
+        customCPUs4: 0,
+        customRAM4: 0,
+        customDisk4: 0,
     };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            shown: true,
+        };
+    }
+
+
+    toggle() {
+        this.setState({
+            shown: !this.state.shown,
+
+        });
+    }
 
     change = e => {
         this.setState({
@@ -35,52 +63,57 @@ export default class FormExampleFieldControl extends Component  {
         this.props.onSubmit(this.state);
     };
 
-    addRow = e => {
-
-    };
 
     render() {
+
+        const hidden = {
+            //display: this.state.shown ? "none" : "block",
+            visibility: this.state.shown ? "hidden" : "visible",
+            height: this.state.shown ? "0px" : "40px",
+            marginBottom: this.state.shown ? "0px" : "12px",
+            marginTop: this.state.shown ? "0px" : "12px"
+        }
         return (
             <Form className="ui segment" style={{borderColor:'#1178ab'}}>
-                <Form.Group widths='equal' >
-                <Form.Input
-                            name="numPowerUsers"
-                            type="number"
-                            min="0"
-                            label="# Users"
-                            placeholder="# Users"
-                            value={this.state.numPowerUsers}
-                            onChange={e => this.change(e)}
-                />
-                <Form.Input data-tooltip="Heavy Resource Consumption" data-position="top left"
-                    label="User Type"
-                    readOnly
-                    value="Power"
-                    placeholder="Last name"
-                    onChange={e => this.change(e)}
-                />
-                <Form.Input
-                    label="# CPUs/User"
-                    readOnly
-                    value="2"
-                    placeholder="CPUs/User"
-                    onChange={e => this.change(e)}
-                />
-                <Form.Input
-                    readOnly
-                    value="8"
-                    label="RAM/User (GB)"
-                    placeholder="RAM/User"
-                    onChange={e => this.change(e)}
-                />
-                <Form.Input
-                    readOnly
-                    value="150"
-                    label="Disk/User (GB)"
-                    placeholder="Disk/User"
-                    onChange={e => this.change(e)}
-                />
-            </Form.Group>
+                <Form.Group widths='equal'>
+                    <Form.Input
+                        name="numPowerUsers"
+                        type="number"
+                        min="0"
+                        label="# Users"
+                        placeholder="# Users"
+                        value={this.state.numPowerUsers}
+                        onChange={e => this.change(e)}
+                    />
+                    <Form.Input data-tooltip="Heavy Resource Consumption" data-position="top left"
+                                label="User Type"
+                                readOnly
+                                value="Power"
+                                placeholder="Last name"
+                                onChange={e => this.change(e)}
+                    />
+                    <Form.Input
+                        label="# CPUs/User"
+                        readOnly
+                        value="2"
+                        placeholder="CPUs/User"
+                        onChange={e => this.change(e)}
+                    />
+                    <Form.Input
+                        readOnly
+                        value="8"
+                        label="RAM/User (GB)"
+                        placeholder="RAM/User"
+                        onChange={e => this.change(e)}
+                    />
+                    <Form.Input
+                        readOnly
+                        value="150"
+                        label="Disk/User (GB)"
+                        placeholder="Disk/User"
+                        onChange={e => this.change(e)}
+                    />
+                </Form.Group>
 
                 <Form.Group widths='equal'>
                     <Form.Input
@@ -150,8 +183,8 @@ export default class FormExampleFieldControl extends Component  {
                         placeholder="Disk/User"
                         onChange={e => this.change(e)}
                     />
-
                 </Form.Group>
+
                 <Form.Group widths='equal'>
                     <Form.Input
                         name="numCustomUsers"
@@ -191,8 +224,130 @@ export default class FormExampleFieldControl extends Component  {
                     />
                 </Form.Group>
 
-                <button style={{backgroundColor: '#cad9de'}} className="circular ui button" data-tooltip="Add up to 3 additional inputs" data-position="top left" onClick={e => this.addRow(e)}>+</button>
+
+                <Form.Group widths='equal' style={hidden}>
+                    <Form.Input
+                        name="numCustomUsers2"
+                        type="number"
+                        min="0"
+                        placeholder="# Users"
+                        value={this.state.numCustomUsers2}
+                        onChange={e => this.change(e)}
+                    />
+                    <Form.Input data-tooltip="Custom Resource Consumption" data-position="top left"
+                                placeholder="Custom"
+                                onChange={e => this.change(e)}
+                    />
+                    <Form.Input
+                        name="customCPUs"
+                        type="number"
+                        min="0"
+                        placeholder="CPUs/User"
+                        value={this.state.customCPUs2}
+                        onChange={e => this.change(e)}
+                    />
+                    <Form.Input
+                        name="customRAM"
+                        type="number"
+                        min="0"
+                        placeholder="RAM/User"
+                        value={this.state.customRAM2}
+                        onChange={e => this.change(e)}
+                    />
+                    <Form.Input
+                        name="customDisk"
+                        type="number"
+                        min="0"
+                        placeholder="Disk/User"
+                        value={this.state.customDisk2}
+                        onChange={e => this.change(e)}
+                    />
+                </Form.Group>
+
+                <Form.Group widths='equal' style={ hidden }>
+                    <Form.Input
+                        name="numCustomUsers3"
+                        type="number"
+                        min="0"
+                        placeholder="# Users"
+                        value={this.state.numCustomUsers3}
+                        onChange={e => this.change(e)}
+                    />
+                    <Form.Input data-tooltip="Custom Resource Consumption" data-position="top left"
+                                placeholder="Custom"
+                                onChange={e => this.change(e)}
+                    />
+                    <Form.Input
+                        name="customCPUs"
+                        type="number"
+                        min="0"
+                        placeholder="CPUs/User"
+                        value={this.state.customCPUs3}
+                        onChange={e => this.change(e)}
+                    />
+                    <Form.Input
+                        name="customRAM"
+                        type="number"
+                        min="0"
+                        placeholder="RAM/User"
+                        value={this.state.customRAM3}
+                        onChange={e => this.change(e)}
+                    />
+                    <Form.Input
+                        name="customDisk"
+                        type="number"
+                        min="0"
+                        placeholder="Disk/User"
+                        value={this.state.customDisk3}
+                        onChange={e => this.change(e)}
+                    />
+                </Form.Group>
+
+
+                <Form.Group style={hidden} widths='equal'  >
+                    <Form.Input
+                        name="numCustomUsers4"
+                        type="number"
+                        min="0"
+                        placeholder="# Users"
+                        value={this.state.numCustomUsers4}
+                        onChange={e => this.change(e)}
+                    />
+                    <Form.Input data-tooltip="Custom Resource Consumption" data-position="top left"
+                                placeholder="Custom"
+                                onChange={e => this.change(e)}
+                    />
+                    <Form.Input
+                        name="customCPUs"
+                        type="number"
+                        min="0"
+                        placeholder="CPUs/User"
+                        value={this.state.customCPUs4}
+                        onChange={e => this.change(e)}
+                    />
+                    <Form.Input
+                        name="customRAM"
+                        type="number"
+                        min="0"
+                        placeholder="RAM/User"
+                        value={this.state.customRAM4}
+                        onChange={e => this.change(e)}
+                    />
+                    <Form.Input
+                        name="customDisk"
+                        type="number"
+                        min="0"
+                        placeholder="Disk/User"
+                        value={this.state.customDisk4}
+                        onChange={e => this.change(e)}
+                    />
+                </Form.Group>
+
+                <button className="circular ui button" data-tooltip="Click to add/remove 3 inputs"
+                        data-position="top left" onClick={this.toggle.bind(this)}>+/-</button>
+
                 <div className="ui divider"></div>
+
                 <Button style={{backgroundColor: '#cad9de'}} onClick={e => this.onSubmit(e)}>Get Estimate</Button>
 
             </Form>
