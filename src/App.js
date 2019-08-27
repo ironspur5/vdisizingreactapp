b991e4e8f8e151f4354aeb112bd8e987fd8a9c48
Luis Ramirez <luis.ram0310@gmail.com>
2019-08-26 20:41:41 -0500

    Updated formatting

--- src/App.js
+++ src/App.js
@@ -108,7 +108,7 @@ class App extends Component {
                 </div>
 
                 <FormExampleFieldControl onSubmit={fields => this.onSubmit(fields)}/>
-                <div className="ui blue segment">
+                <div className="ui segment" style={{borderColor:'#1178ab'}}>
                 <Header>Total Requirements</Header>
 
                 <table className="ui celled table" >
